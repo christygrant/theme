77fe08b393e6573901fdd8f2268ad5d0ca7a500b
Brian Bonnlander <bonnland@ucar.edu>
2016-08-26 07:40:50 -0400

    Update CKAN 'Author' field with ISO xml authors

--- ckanext/dset_harvester/plugin.py
+++ ckanext/dset_harvester/plugin.py
@@ -1,9 +1,48 @@
-import ckan.plugins as plugins
+import ckan.plugins as p
 import ckan.plugins.toolkit as toolkit
 
 
-class Dset_HarvesterPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+from ckanext.spatial.interfaces import ISpatialHarvester
+
+# Debug
+import logging
+from pprint import pprint
+
+log = logging.getLogger(__name__)
+
+ISO_NAMES = {'gmd': 'http://www.isotc211.org/2005/gmd',
+             'xlink': 'http://www.w3.org/1999/xlink',
+             'gco': 'http://www.isotc211.org/2005/gco',
+             'gml': 'http://www.opengis.net/gml'}
+
+class Dset_HarvesterPlugin(p.SingletonPlugin):
+    p.implements(ISpatialHarvester, inherit=True)
+    p.implements(p.IConfigurer)
+
+    # ISpatialHarvester
+
+    def get_package_dict(self, context, data_dict):
+        package_dict = data_dict['package_dict']
+        iso_values = data_dict['iso_values']
+        xml_tree = data_dict['xml_tree']
+
+        # Add ISO Topic Category from spatial harvester 
+        package_dict['extras'].append(
+            {'key': 'topic-category', 'value': iso_values.get('topic-category')}
+        )
+	
+        # Add Authors by searching the ISO XML for "cited responsible parties" with an "author" role. 
+	authorList = []
+        authorPathXML = './/gmd:citedResponsibleParty/gmd:CI_ResponsibleParty'
+        for party in xml_tree.xpath(authorPathXML, namespaces=ISO_NAMES):
+	    roleString = party.findtext('gmd:role/gmd:CI_RoleCode', namespaces=ISO_NAMES)
+            log.debug('roleString == "' + roleString + '"')
+            if roleString == 'author':
+	        authorList.append(party.findtext('gmd:individualName/gco:CharacterString', namespaces=ISO_NAMES))
+	
+        package_dict['author'] = authorList
+
+        return package_dict
 
     # IConfigurer
 
@@ -11,3 +50,5 @@ class Dset_HarvesterPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'dset_harvester')
+
+
